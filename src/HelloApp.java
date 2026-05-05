c3489aeab0ba6d7a81cfb4da9cb987af3f131b8a
jaswanthreddydornala875-arch <jaswanthreddydornala875@gmail.com>
2026-05-06 01:45:28 +0530

    UC3

--- src/HelloApp.java
+++ src/HelloApp.java
@@ -1,8 +1,6 @@
 public class HelloApp{
     public static void main(String[]args){
-        // here yourname is taken in args array with index 0
-        //and it is taken when you pass java filename Name in command/terminal.
-        String name = args[0];
+        String name = args.length>0?args[0]:"World";
         System.out.println("Hello, "+ name +"!");
     }
 }
