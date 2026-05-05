055ec1ead8212f8a2dfdb4a460ba2ae4bbde58b6
jaswanthreddydornala875-arch <jaswanthreddydornala875@gmail.com>
2026-05-06 01:54:38 +0530

    UC4

--- src/HelloApp.java
+++ src/HelloApp.java
@@ -1,6 +1,18 @@
-public class HelloApp{
-    public static void main(String[]args){
-        String name = args.length>0?args[0]:"World";
-        System.out.println("Hello, "+ name +"!");
+public class HelloApp {
+    public static void main(String[] args) {
+        String name = "World";
+
+        if (args.length > 0) {
+            StringBuilder nameBuilder = new StringBuilder();
+            for (int i = 0; i < args.length; i++) {
+                nameBuilder.append(args[i]);
+                if (i < args.length - 1) {
+                    nameBuilder.append(", ");
+                }
+            }
+            name = nameBuilder.toString();
+        }
+
+        System.out.println("Hello, " + name + "!");
     }
 }
